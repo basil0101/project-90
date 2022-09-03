cf4941015e283363532e7ff925be6a6e28335121
basil0101 <94240781+basil0101@users.noreply.github.com>
2022-09-03 09:08:44 -0500

    Add files via upload

---
+++  gamepage.js
@@ -0,0 +1,6 @@
+player1_name=localStorage.getItem("player1_name");
+player2_name=localStorage.getItem("player2_name");
+
+
+document.getElementById("player1_name").innerHTML=player1_name+" : ";
+document.getElementById("player2_name").innerHTML=player2_name+" : ";
